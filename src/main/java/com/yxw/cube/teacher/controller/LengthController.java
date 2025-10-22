dc0e92324e4d238666c748bc2eab6272a4574d3b
xiaowyang <xiaowyang@deloitte.com.cn>
2025-10-22 16:37:03 +0800

    add length share

---
+++ src/main/java/com/yxw/cube/teacher/controller/LengthController.java
@@ -0,0 +1,13 @@
+package com.yxw.cube.teacher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LengthController {
+
+    @GetMapping("/length")
+    public String length() {
+        return "length";
+    }
+}
